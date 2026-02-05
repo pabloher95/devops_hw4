704833ce74ca70960e6e060dca09c233d9bf7428
pabloher95 <phernandezpedraz@uchicago.edu>
2026-02-04 19:07:52 -0600

    bug in schema sql

--- staging_schema.sql
+++ staging_schema.sql
@@ -4,7 +4,7 @@ USE staging_db;
 
 DROP TABLE IF EXISTS to_do;
 
-CREATE TABLE to do (
+CREATE TABLE to_do (
     task_id INT AUTO_INCREMENT PRIMARY KEY, 
     task VARCHAR(120) NOT NULL,
     task_status VARCHAR(120) NOT NULL default 'PENDING'
